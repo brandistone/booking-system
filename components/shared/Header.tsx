a4f2b63f93a76401b2f5d3b8e0dd98c63b39e23f
brandistone <brandymabeya@gmail.com>
2025-01-09 21:18:14 +0300

    height changes

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,8 +6,13 @@ const Header = () => {
     <header className='w-full boarder-b'>
         <div className='wrapper flex items-center justify-between'>
             <Link href={'/'} className='w-36'>
-                <Image src="/assets/images/logo.svg" />
+                <img src="/logo.svg" width={128} height={38} alt="Brandy logo" 
+                />
             </Link>
+            <div className='flex w-32 justify-end gap-3'>
+              
+
+            </div>
            
 
         </div>
