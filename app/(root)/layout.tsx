dfba688cd183898c156f4f29a41c161548bcb56a
brandistone <brandymabeya@gmail.com>
2025-01-09 18:07:35 +0300

    Header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/shared/Footer"
+import Header from "@/components/shared/Header"
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+     
+        <html lang="en">
+          <Header />
+          <body>{children}</body>
+          <Footer />
+        </html>
+      
+    )
+  }
