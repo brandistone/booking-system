d0bfb324ac154569c0368bef98238de14d87c72b
brandistone <brandymabeya@gmail.com>
2025-01-10 15:40:07 +0300

    adding dependencies

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,7 +2,8 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
-
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -17,14 +18,14 @@ const Header = () => {
 
         <SignedIn>
           <nav className="md:flex-between hidden w-full max-w-xs">
-          
+            <NavItems />
           </nav>
         </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
-           
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
+
+const MobileNav = () => {
+  return (
+    <div>MobileNav</div>
+  )
+}
+
+export default MobileNav
