f6e5ff0992f96f712a31c3d5d8972f81c942530d
brandistone <brandymabeya@gmail.com>
2025-01-10 15:24:19 +0300

    authentication

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,29 +1,40 @@
-import { SignedOut } from '@clerk/nextjs'
-import { Link } from 'lucide-react'
-import React from 'react'
-import { Button } from '../ui/button'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
 
 const Header = () => {
   return (
-    <header className='w-full boarder-b'>
-        <div className='wrapper flex items-center justify-between'>
-            <Link href={'/'} className='w-36'>
-                <img src="/logo.svg" width={128} height={38} alt="Brandy logo" 
-                />
-            </Link>
-            <div className='flex w-32 justify-end gap-3'>
-              <SignedOut>
-                <Button asChild className='rounded-full' size='lg'>
-                  <Link href="/sign-in">
-                    Login
-                  </Link>
-                </Button>
-              </SignedOut>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
+          />
+        </Link>
 
-            </div>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+          
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
            
-
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
+      </div>
     </header>
   )
 }
